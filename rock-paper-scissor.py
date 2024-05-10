1c085fc74462df75216b63a30be1d9b96371ba12
Rutarj <shahrutarj@gmail.com>
2024-05-10 15:22:24 -0400

    file added

---
+++ rock-paper-scissor.py
@@ -0,0 +1,68 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+
+import random
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+print(f"user_choice {user_choice}")
+if user_choice == 0:
+    print(rock)
+elif user_choice == 1:
+    print(paper)
+else:
+    print(scissors)
+
+computer_choice = random.randint(0,2)
+print(f"Computer choice {computer_choice}")
+if computer_choice == 0:
+    print(rock)
+elif computer_choice == 1:
+    print(paper)
+else:
+    print(scissors)
+
+if computer_choice == user_choice:
+    print("It's a draw")
+
+elif computer_choice == 0 and user_choice == 1:
+    print("You win")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose")
+elif computer_choice == 1 and user_choice == 0:
+    print("You lose")
+elif computer_choice == 1 and user_choice == 2:
+    print("You win")
+elif computer_choice == 2 and user_choice == 0:
+    print("You win")
+elif computer_choice == 2 and user_choice == 1:
+    print("You lose")
+else:
+    print("You typed an invalid number, you lose!")
+
+
