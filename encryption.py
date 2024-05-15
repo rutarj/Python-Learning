97e6e849aeddd1197611ac6cfd7352e32989a43d
Rutarj <shahrutarj@gmail.com>
2024-05-15 17:53:38 -0400

    encryption file added

--- encryption.py
+++ encryption.py
@@ -1,16 +1,16 @@
-- The Python code implements a Caesar cipher, a technique for encryption and decryption.
-- The alphabet list is initialized, containing lowercase letters duplicated for boundary handling.
-- User inputs include the cipher direction (encoding or decoding), the text to process, and the shift amount.
-- The core `caesar()` function handles encryption and decryption.
-- It iterates over each letter, calculates the new position after shifting, and appends the result.
-- Shift direction adjusts based on the user's choice, with the final result printed.
+# - The Python code implements a Caesar cipher, a technique for encryption and decryption.
+# - The alphabet list is initialized, containing lowercase letters duplicated for boundary handling.
+# - User inputs include the cipher direction (encoding or decoding), the text to process, and the shift amount.
+# - The core `caesar()` function handles encryption and decryption.
+# - It iterates over each letter, calculates the new position after shifting, and appends the result.
+# - Shift direction adjusts based on the user's choice, with the final result printed.
 
-- In the function call, `caesar()` is invoked with user-provided inputs.
-- Depending on the direction, the function encrypts or decrypts the text.
-- Encryption shifts each letter forward by the specified amount.
-- Decryption reverses the shift by moving letters backward.
-- The modulo operator ensures shifts wrap around the alphabet's bounds.
-- The code offers a simple yet effective demonstration of the Caesar cipher technique.
+# - In the function call, `caesar()` is invoked with user-provided inputs.
+# - Depending on the direction, the function encrypts or decrypts the text.
+# - Encryption shifts each letter forward by the specified amount.
+# - Decryption reverses the shift by moving letters backward.
+# - The modulo operator ensures shifts wrap around the alphabet's bounds.
+# - The code offers a simple yet effective demonstration of the Caesar cipher technique.
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
